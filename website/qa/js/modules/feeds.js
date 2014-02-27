72ecd0295dcdbd5d2bfac621b92ec5022ba94164
MrLuckyWaffles <mrluckywaffles@gmail.com>
2014-02-27 15:38:08 -0500

    update params ref

--- website/qa/js/modules/feeds.js
+++ website/qa/js/modules/feeds.js
@@ -82,7 +82,7 @@
 			checkRSS(feeds.slice(1), callback, failureHandler);
 		};
 	
-		var param = getParam("rss");	
+		var param = _tools.getParam("rss");	
 		if(param === "false")
 		{
 			checkum(null);
