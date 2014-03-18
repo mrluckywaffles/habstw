e92a3cf321e843bf46348d55d990852ea58f1d3f
MrLuckyWaffles <mrluckywaffles@gmail.com>
2014-03-18 16:41:59 -0400

    update src to use new set method

--- website/qa/js/modules/src/src.js
+++ website/qa/js/modules/src/src.js
@@ -2,13 +2,13 @@
 
 	var _tools = anime.module('tools');
   
-  var moduleName = 'src_klk';
+	var moduleName = 'src_klk';
   
-  var paramSrc = _tools.getParam('src');
-  if(paramSrc && paramSrc.length > 0){
-    moduleName = 'src_' + paramSrc;
-  }
-
-  module = anime.module(moduleName);
+	var paramSrc = _tools.getParam('src');
+	if(paramSrc && paramSrc.length > 0){
+		moduleName = 'src_' + paramSrc;
+	}
+	
+	anime.module.set(module.moduleName, moduleName);
 
 })(anime.module('src'));
