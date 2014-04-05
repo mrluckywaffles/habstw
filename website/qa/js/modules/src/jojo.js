6f16cb1979d5b8700a411d06522aca4de19bbae5
MrLuckyWaffles <mrluckywaffles@gmail.com>
2014-04-05 14:13:57 -0400

    updated jojo src with feed and images

--- website/qa/js/modules/src/jojo.js
+++ website/qa/js/modules/src/jojo.js
@@ -7,16 +7,17 @@
 	var feeds = {};
 	var text = {};
 
-	images.loadingRss = 'http://i.imgur.com/XsOXgn5.gif'; //bobbing ryuuko
-	images.leftAnswerBookend = 'http://i.imgur.com/HNR6h13.png'; //mako face right
-	images.rightAnswerBookend = 'http://i.imgur.com/ShLuqus.png'; //mako face left
+	images.loadingRss = 'http://i.imgur.com/QzEuoVY.gif'; //dio stand punching
+	images.leftAnswerBookend = 'http://i.imgur.com/CpXcYP4.gif'; //dio standing
+	images.rightAnswerBookend = 'http://i.imgur.com/vbsR9w0.gif'; //jotaro win
 	images.waiting = [
 		'http://i.imgur.com/Il557U0.png'	//dio vs jotaro art
 	];
 	images.success = [
-		'http://i.imgur.com/pTX2Bz4.png'  //ED-2 mako + elephant
+		'http://i.imgur.com/QFwkov1.gif'  //YES gif
 	];
 	images.defaultMainImageTag = '<img src="http://i.imgur.com/Il557U0.png" style="width: 700px;">';
+		//jojo and dio face off
 	
 	text.waiting = "soon...";
 
@@ -24,7 +25,14 @@
 		'http://k007.kiwi6.com/hotlink/10po8om1ml'		//roundabout
 	];
 
-	feeds.feeds = [];
+	feeds.feeds = [
+		_tools.rawFeed(
+			'HORRIBLE',
+			'http://www.nyaa.se/?page=rss&user=64513&term=JoJo',
+			'http://horriblesubs.info/',
+			'http://www.nyaa.se/?page=search&cats=0_0&filter=0&term=JoJo&user=64513'
+		)
+	];
 
 	module.images = images;
 	module.text = text;
