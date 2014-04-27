c3ca5fa12142aee031b34dd722ef33bba4e98530
MrLuckyWaffles <mrluckywaffles@gmail.com>
2014-04-27 13:18:24 -0400

    ftp now takes min args

--- ftp.sh
+++ ftp.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-./minify.sh
+minify=0
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -m | --minify )         minify=1
+                                ;;
+    esac
+    shift
+done
+
+
+if [ "$minify" = "1" ]; then
+	./minify.sh
+fi
 
 scp -r website/* mpaulweeks_hasanimebeensavedthisweek@ssh.phx.nearlyfreespeech.net:/home/public
